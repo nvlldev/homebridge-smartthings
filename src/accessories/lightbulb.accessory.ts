62809fcc69cd4d6218857ea968f7ea26033ac2d0
Scott Petersen <scott@kodashi.me>
2022-05-14 12:20:15 -0600

    Added Color Temperature Control

--- src/accessories/lightbulb.accessory.ts
+++ src/accessories/lightbulb.accessory.ts
@@ -31,10 +31,28 @@ export class LightbulbAccessory extends BaseAccessory {
         .getCharacteristic(this.Characteristic.Brightness)
         .onSet(this.setLevel.bind(this))
         .onGet(this.getLevel.bind(this));
+
+    if (this.hasCapability("colorControl")) {
+      this.service
+        .getCharacteristic(this.Characteristic.Hue)
+        .onSet(this.setHue.bind(this))
+        .onGet(this.getHue.bind(this));
+
+      this.service
+        .getCharacteristic(this.Characteristic.Saturation)
+        .onSet(this.setSaturation.bind(this))
+        .onGet(this.getSaturation.bind(this));
+    }
+
+    if (this.hasCapability("colorTemperature"))
+      this.service
+        .getCharacteristic(this.Characteristic.ColorTemperature)
+        .onSet(this.setColorTemperature.bind(this))
+        .onGet(this.getColorTemperature.bind(this));
   }
 
   private async setOn(value: CharacteristicValue): Promise<void> {
-    this.log.debug(`Received onSet('${value}') Event for ${this.name}`);
+    this.log.debug(`Received setOn(${value}) Event for ${this.name}`);
 
     return new Promise<void>((resolve, reject) => {
       if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
@@ -45,12 +63,12 @@ export class LightbulbAccessory extends BaseAccessory {
           command: value ? "on" : "off",
         })
         .then((response) => {
-          this.log.debug(`onSet('${value}') Successful for ${this.name}`);
+          this.log.debug(`setOn(${value}) Successful for ${this.name}`);
           resolve();
         })
         .catch((error) => {
           this.log.error(
-            `onSet Failed for ${this.name}. [Communication Error]`,
+            `setOn Failed for ${this.name}. [Communication Error]`,
             error
           );
           reject(
@@ -64,7 +82,7 @@ export class LightbulbAccessory extends BaseAccessory {
 
   private async getOn(): Promise<CharacteristicValue> {
     let onStatus = 0;
-    this.log.debug(`Received onGet() Event for ${this.name}`);
+    this.log.debug(`Received getOn() Event for ${this.name}`);
 
     return new Promise<CharacteristicValue>((resolve, reject) => {
       if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
@@ -76,7 +94,7 @@ export class LightbulbAccessory extends BaseAccessory {
             const value = status.components!.main.switch.switch.value;
 
             this.log.debug(
-              `onGet() Successful for ${this.name}. Value: '${value}'`
+              `getOn() Successful for ${this.name}. Value: '${value}'`
             );
 
             onStatus = value === "on" ? 1 : 0;
@@ -84,7 +102,7 @@ export class LightbulbAccessory extends BaseAccessory {
             resolve(onStatus);
           } else {
             this.log.debug(
-              `onGet() Failed for ${this.name}. [Undefined Value]`
+              `getOn() Failed for ${this.name}. [Undefined Value]`
             );
             reject(
               new this.hap.HapStatusError(
@@ -95,7 +113,7 @@ export class LightbulbAccessory extends BaseAccessory {
         })
         .catch((error) => {
           this.log.debug(
-            `onGet() Failed for ${this.name}. [Communication Error]`,
+            `getOn() Failed for ${this.name}. [Communication Error]`,
             error
           );
           reject(
@@ -108,7 +126,7 @@ export class LightbulbAccessory extends BaseAccessory {
   }
 
   private async setLevel(value: CharacteristicValue): Promise<void> {
-    this.log.debug(`Received setLevel() Event for ${this.name}`);
+    this.log.debug(`Received setLevel(${value}) Event for ${this.name}`);
 
     return new Promise<void>((resolve, reject) => {
       if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
@@ -120,7 +138,7 @@ export class LightbulbAccessory extends BaseAccessory {
           arguments: [value as number],
         })
         .then((response) => {
-          this.log.debug(`setLevel('${value}') Successful for ${this.name}`);
+          this.log.debug(`setLevel(${value}) Successful for ${this.name}`);
           resolve();
         })
         .catch((error) => {
@@ -139,7 +157,7 @@ export class LightbulbAccessory extends BaseAccessory {
 
   private async getLevel(): Promise<CharacteristicValue> {
     let level = 0;
-    this.log.debug(`Received onGet() Event for ${this.name}`);
+    this.log.debug(`Received getLevel() Event for ${this.name}`);
 
     return new Promise<CharacteristicValue>((resolve, reject) => {
       if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
@@ -169,6 +187,236 @@ export class LightbulbAccessory extends BaseAccessory {
         .catch((error) => {
           this.log.debug(
             `getLevel() Failed for ${this.name}. [Communication Error]`,
+            error
+          );
+          reject(
+            new this.hap.HapStatusError(
+              this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+            )
+          );
+        });
+    });
+  }
+
+  private async setHue(value: CharacteristicValue): Promise<void> {
+    this.log.debug(`Received setHue(${value}) Event for ${this.name}`);
+
+    return new Promise<void>((resolve, reject) => {
+      if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
+
+      this.client.devices
+        .executeCommand(this.id, {
+          capability: "colorControl",
+          command: "setHue",
+          arguments: [((value as number) / 360) * 100],
+        })
+        .then((response) => {
+          this.log.debug(`setHue(${value}) Successful for ${this.name}`);
+          resolve();
+        })
+        .catch((error) => {
+          this.log.error(
+            `setHue Failed for ${this.name}. [Communication Error]`,
+            error
+          );
+          reject(
+            new this.hap.HapStatusError(
+              this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+            )
+          );
+        });
+    });
+  }
+
+  private async getHue(): Promise<CharacteristicValue> {
+    let hue = 0;
+    this.log.debug(`Received getHue() Event for ${this.name}`);
+
+    return new Promise<CharacteristicValue>((resolve, reject) => {
+      if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
+
+      this.client.devices
+        .getStatus(this.id)
+        .then((status) => {
+          if (status.components!.main.switch.switch.value) {
+            hue =
+              ((status.components!.main.colorControl.hue.value as number) /
+                100) *
+              360;
+
+            this.log.debug(
+              `getHue() Successful for ${this.name}. Value: '${hue}'`
+            );
+
+            resolve(hue);
+          } else {
+            this.log.debug(
+              `getHue() Failed for ${this.name}. [Undefined Value]`
+            );
+            reject(
+              new this.hap.HapStatusError(
+                this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+              )
+            );
+          }
+        })
+        .catch((error) => {
+          this.log.debug(
+            `getHue() Failed for ${this.name}. [Communication Error]`,
+            error
+          );
+          reject(
+            new this.hap.HapStatusError(
+              this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+            )
+          );
+        });
+    });
+  }
+
+  private async setSaturation(value: CharacteristicValue): Promise<void> {
+    this.log.debug(`Received setSaturation(${value}) Event for ${this.name}`);
+
+    return new Promise<void>((resolve, reject) => {
+      if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
+
+      this.client.devices
+        .executeCommand(this.id, {
+          capability: "colorControl",
+          command: "setSaturation",
+          arguments: [value as number],
+        })
+        .then((response) => {
+          this.log.debug(
+            `setSaturation(${value}) Successful for ${this.name}`
+          );
+          resolve();
+        })
+        .catch((error) => {
+          this.log.error(
+            `setSaturation Failed for ${this.name}. [Communication Error]`,
+            error
+          );
+          reject(
+            new this.hap.HapStatusError(
+              this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+            )
+          );
+        });
+    });
+  }
+
+  private async getSaturation(): Promise<CharacteristicValue> {
+    let saturation = 0;
+    this.log.debug(`Received getSaturation() Event for ${this.name}`);
+
+    return new Promise<CharacteristicValue>((resolve, reject) => {
+      if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
+
+      this.client.devices
+        .getStatus(this.id)
+        .then((status) => {
+          if (status.components!.main.switch.switch.value) {
+            saturation = status.components!.main.colorControl.saturation
+              .value as number;
+
+            this.log.debug(
+              `getSaturation() Successful for ${this.name}. Value: '${saturation}'`
+            );
+
+            resolve(saturation);
+          } else {
+            this.log.debug(
+              `getSaturation() Failed for ${this.name}. [Undefined Value]`
+            );
+            reject(
+              new this.hap.HapStatusError(
+                this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+              )
+            );
+          }
+        })
+        .catch((error) => {
+          this.log.debug(
+            `getSaturation() Failed for ${this.name}. [Communication Error]`,
+            error
+          );
+          reject(
+            new this.hap.HapStatusError(
+              this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+            )
+          );
+        });
+    });
+  }
+
+  private async setColorTemperature(value: CharacteristicValue): Promise<void> {
+    this.log.debug(`Received setColorTemperature(${value}) Event for ${this.name}`);
+
+    return new Promise<void>((resolve, reject) => {
+      if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
+
+      this.client.devices
+        .executeCommand(this.id, {
+          capability: "colorTemperature",
+          command: "setColorTemperature",
+          arguments: [Math.floor(1000000 / (value as number))],
+        })
+        .then((response) => {
+          this.log.debug(
+            `setColorTemperature(${value}) Successful for ${this.name}`
+          );
+          resolve();
+        })
+        .catch((error) => {
+          this.log.error(
+            `setColorTemperature Failed for ${this.name}. [Communication Error]`,
+            error
+          );
+          reject(
+            new this.hap.HapStatusError(
+              this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+            )
+          );
+        });
+    });
+  }
+
+  private async getColorTemperature(): Promise<CharacteristicValue> {
+    let colorTemperature = 0;
+    this.log.debug(`Received getColorTemperature() Event for ${this.name}`);
+
+    return new Promise<CharacteristicValue>((resolve, reject) => {
+      if (!this.isOnline()) this.rejectPromiseDeviceOffline(reject);
+
+      this.client.devices
+        .getStatus(this.id)
+        .then((status) => {
+          if (status.components!.main.switch.switch.value) {
+            colorTemperature =
+              Math.floor(1000000 /
+              (status.components!.main.colorTemperature.colorTemperature
+                .value as number));
+
+            this.log.debug(
+              `getColorTemperature() Successful for ${this.name}. Value: '${colorTemperature}'`
+            );
+
+            resolve(colorTemperature);
+          } else {
+            this.log.debug(
+              `getColorTemperature() Failed for ${this.name}. [Undefined Value]`
+            );
+            reject(
+              new this.hap.HapStatusError(
+                this.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE
+              )
+            );
+          }
+        })
+        .catch((error) => {
+          this.log.debug(
+            `getColorTemperature() Failed for ${this.name}. [Communication Error]`,
             error
           );
           reject(
